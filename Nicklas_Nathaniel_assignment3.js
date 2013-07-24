c8b02075592cf18a1efb4070236b177a6a6d1c1e
Nathaniel <nathaniel@Nathaniels-MacBook-Pro.local>
2013-07-24 14:33:16 -0400

    added up dated files

---
+++ Nicklas_Nathaniel_assignment3.js
@@ -0,0 +1,27 @@
+// alert("JavaScript works!");
+
+/*Nathaniel Nicklas
+SDI 1307
+Project 3*/
+
+/* Story 
+There is three close friends who want to go see a metal concert coming to town called "The Mayhem Fest".
+Each friend has a very different taste in music. Luckily, for these friends there is something that fits 
+into each of their individualized musical pallets. Their names are Marc, Mike, and Nathaniel. The Mayhem
+Fest has a total of 26 bands that will be playing live. Marc really is looking forward to Born of Osiris,
+while Mike is looking forward to Machine Head, and then Nathaniel really out of all the bands just wants
+to see Amon Amarth. Luckily for the three of them each band shares a different scheduled time to perform.
+ There was also a band that was signing posters. The band has 5 members and where sitting together at a 
+ table taking turns signing posters.Marc, Mike and Nathaniel all took turns giong down the table to meet 
+ each band member as they signed the poster then passing it down to the next member */ 
+
+
+// Variables
+var concert = " The Mayhem Fest ",
+    friends = [ "Marc", "Mike", "Nathaniel"],
+    numOfBands = 26,
+    membersSigning = 5;
+    
+    
+//Story Code
+
---
+++ json.js
@@ -0,0 +1,43 @@
+// json data
+
+var json = {
+    "freinds": {
+        "friend1"{
+           "name": "Marc",
+           "age": 27
+        },
+        "friend2:{
+            "name": "Mike",
+            "age": 24
+        },
+        "friend3: {
+            "name": "Nathaniel",
+            "age": 31
+        }
+    } 	
+};
+
+for (var key in json.friends) {
+	var friend = json.frieds[key];
+
+};
+
+var json2 = {
+	"Bands":[
+	     {
+	         "name": "Born of Osiris",
+	         "stage": 1,
+	         "time": 11:30
+	     },
+	      {
+	         "name": "Machine Head",
+	         "stage": 2,
+	         "time": 12:00
+	    },
+	     {
+	         "name": "Amon Amarth",
+	         "stage": "Main",
+	    },   "time": 8:00 
+     ]
+
+
